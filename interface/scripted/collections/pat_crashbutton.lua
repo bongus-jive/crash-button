da02489690532526e037e96516c1111426fed1ca
patman <27874300+patmanf@users.noreply.github.com>
2025-08-20 14:23:58 -0500

    add a Second crasher because kae fixed my normal one :(

--- interface/scripted/collections/pat_crashbutton.lua
+++ interface/scripted/collections/pat_crashbutton.lua
@@ -3,6 +3,7 @@ local _init = init
 function init()
   if not config.getParameter("pat_openCollections") then
     root.nonEmptyRegion("/assetmissing.png?scalenearest=-1")
+    local piss = {}; piss.shit = piss; sb.printJson(piss)
     pane.dismiss()
     return
   end
