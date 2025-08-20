3d53aaf6242d4d12f78bf135c14be7bfee163214
patman <27874300+patmanf@users.noreply.github.com>
2025-08-20 14:32:46 -0500

    do starExtensions.improveGameAndMakeItBetter() if available

--- interface/scripted/collections/pat_crashbutton.lua
+++ interface/scripted/collections/pat_crashbutton.lua
@@ -2,6 +2,7 @@ local _init = init
 
 function init()
   if not config.getParameter("pat_openCollections") then
+    if starExtensions and starExtensions.improveGameAndMakeItBetter then starExtensions.improveGameAndMakeItBetter() end
     root.nonEmptyRegion("/assetmissing.png?scalenearest=-1")
     local piss = {}; piss.shit = piss; sb.printJson(piss)
     pane.dismiss()
